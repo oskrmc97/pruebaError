a56eb46e08418a974ad5ae9cef6ed91b4a174c3a
Oscar Melo <oskr975@gmail.com>
2023-06-10 09:45:43 -0500

    prueba Errores expeciotion

---
+++ src/main/java/com/semillero/tiendaComic/AppComics.java
@@ -0,0 +1,13 @@
+package com.semillero.tiendaComic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppComics {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppComics.class, args);
+	}
+
+}
